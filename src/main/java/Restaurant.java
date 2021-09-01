d8e5bc051662c291a9ab31051de9f7cd920acfa7
srahuf <srahuf@gmail.com>
2021-09-02 01:50:52 +0530

    Part 3: Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -79,7 +79,15 @@ public class Restaurant {
     public String getName() {
         return name;
     }
-
-
-
+    public int totalOrderValue(List<String> itemNames) throws itemNotFoundException {
+        int orderValue = 0;
+        for (String itemName : itemNames) {
+            Item item = findItemByName(itemName);
+            if (item == null) {
+                throw new itemNotFoundException(itemName);
+            }
+            orderValue += item.getPrice();
+        }
+        return orderValue;
+    }
 }
